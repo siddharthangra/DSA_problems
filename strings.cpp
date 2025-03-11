7ebf21e07297d3a8adf503943da4094b239991ca
siddharthangra <siddharthangra@gmail.com>
2025-03-11 13:02:15 +0530

    odd number  in string

--- strings.cpp
+++ strings.cpp
@@ -34,10 +34,32 @@ string reverse_words(string input){
     }
     return result;
 }
+
+//Q2 Largest odd number in the string
+string largestOddNumber(string num) {
+    int ans = 0;
+    int n = num.length();
+    for(int i =0;i<n;i++){
+        for(int j = i;j<n;j++){
+            string sub_string = num.substr(i,j);
+            int temp = stoi(sub_string);
+            if( temp % 2 == 1){
+                if(ans < temp){ ans = temp;}
+                else{continue;}
+            }else{continue;}
+        }
+    }
+    if(ans != 0){return to_string(ans);}
+    else{return "";}
+};
+
+
 int main(){
     string hello = "Hello my name is raj";
     string reversed_hello = reverse(hello);
     string resultant = reverse_words(reversed_hello);
-    print_string(resultant);
+    //print_string(resultant);
+    string ans  = largestOddNumber("34528");
+    print_string(ans);
 
 }
